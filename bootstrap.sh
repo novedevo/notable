bcb20a0bfa71445b5e16e10d192382953870f434
Devon Sawatsky <novedevo@gmail.com>
2022-06-26 13:48:28 -0700

    fix sql initialization script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,6 @@
 
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y postgresql
-sudo -u postgres psql -f bootstrap.sql
+sudo service postgresql start
+sudo -u postgres psql -c "CREATE DATABASE notable;"
+sudo -u postgres psql -f bootstrap.sql -d notable
