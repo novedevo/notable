38d73cb4ce1fe8dd72ae612f09857297f3437b3c
jamie-chin <jamieleechin@gmail.com>
2022-06-27 20:38:18 -0700

    fixed a comma in sql bootstrap

--- bootstrap.sql
+++ bootstrap.sql
@@ -2,6 +2,6 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT NOT NULL,
     password TEXT NOT NULL,
-    name TEXT NOT NULL,
+    name TEXT NOT NULL
 );
 ALTER USER postgres PASSWORD 'postgres';
