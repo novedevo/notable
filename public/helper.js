a65c76498915755c67b2f0d9ad09d35319289da0
tchua <tchua@sfu.ca>
2022-06-28 19:43:11 -0700

    Added YouTube API functionality. URL entry works. Still needs getTime() and goToTime()

--- public/helper.js
+++ public/helper.js
@@ -1,13 +1,39 @@
-var currentVideo = ""; // Holds the URL being used currently.
+var videoURL; // Holds the URL being used currently.
+
+// This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// This function creates an <iframe> (and YouTube player)
+// after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: '',
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+// The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+// This literally does nothing, but the iframe breaks without it.
+function onPlayerStateChange(event) {}
 
 // Method takes URL input, displays video if valid.
 function setVideo(){
-
-  var urlPrefix = "https://www.youtube.com/embed/"
-  var url = getId(document.getElementById("videoForm").value) ; // Get unique video ID from form.
-  currentVideo = urlPrefix+url; // Set as current video URL for use.
-  document.getElementById("videoDisplay").src = currentVideo; // Set video.
-  document.getElementById("demo").innerHTML = currentVideo; // Debug visual confirmation
+  videoURL = getId(document.getElementById("videoForm").value) ; // Get ID from form.
+  player.loadVideoById(videoURL);
   document.getElementById("videoForm").value = ""; // Reset form.
 }
 
