b5252a148506742e9eb17e78cda5260c278c0637
Devon Sawatsky <novedevo@gmail.com>
2022-07-03 15:27:58 -0700

    update bootstrap scripts

--- bootstrap.sql
+++ bootstrap.sql
@@ -1,7 +1,12 @@
+DROP TABLE users;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT NOT NULL,
     password TEXT NOT NULL,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    admin BOOLEAN NOT NULL DEFAULT FALSE
 );
-ALTER USER postgres PASSWORD 'postgres';
+INSERT INTO users (username, password, name, admin)
+VALUES ('admin', 'admin', 'Admin', TRUE);
+INSERT INTO users (username, password, name, admin)
+VALUES ('user', 'user', 'User', FALSE);
