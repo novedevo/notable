0030b7e3890b41065ccaecac9d1ce588cc313b24
jamie-chin <jamieleechin@gmail.com>
2022-07-02 20:07:19 -0700

    adding a notes table  sql command in bootstrap and a non-functional localstorage garbagedump in stopwatch

--- bootstrap.sql
+++ bootstrap.sql
@@ -1,7 +1,13 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username TEXT NOT NULL,
     password TEXT NOT NULL,
     name TEXT NOT NULL
 );
 ALTER USER postgres PASSWORD 'postgres';
+
+CREATE TABLE IF NOT EXISTS notes (
+    note_id SERIAL PRIMARY KEY,
+    note TEXT,
+    notetaker_id integer REFERENCES users (id)
+);
--- public/stopwatch.js
+++ public/stopwatch.js
@@ -14,19 +14,82 @@ document.getElementById("stopTimer").addEventListener("click", () => {
 });
 
 function calculateTimer() {
-  seconds += 1;
-  if (seconds == 60) {
-    seconds = 0;
-    minutes++;
-    if (minutes == 60) {
-      minutes = 0;
-      hours++;
-    }
-  }
+	seconds += 1;
+	if (seconds == 60) {
+		seconds = 0;
+		minutes++;
+		if (minutes == 60) {
+			minutes = 0;
+			hours++;
+		}
+	}
 
-  let h = hours < 10 ? "0" + hours : hours;
-  let m = minutes < 10 ? "0" + minutes : minutes;
-  let s = seconds < 10 ? "0" + seconds : seconds;
+	let h = hours < 10 ? "0" + hours : hours;
+	let m = minutes < 10 ? "0" + minutes : minutes;
+	let s = seconds < 10 ? "0" + seconds : seconds;
 
-  timerHtml.innerHTML = ` ${h} : ${m} : ${s}`;
+	timerHtml.innerHTML = `${h}:${m}:${s}`;
 }
+
+/////////////////////// Notes posting functions /////////////////////
+/////////////////////// Notes posting functions /////////////////////
+
+// trigger post button click on enter
+function postOnEnter() {
+	if (window.event.key === "Enter") {
+		window.event.preventDefault();
+		post();
+	}
+}
+
+const inputNotes = document.getElementById("inputnotes");
+const notesDisplay = document.getElementById("notesdisplay");
+const displayTime = document.getElementById("displayTime");
+
+let notesStorage = localStorage.getItem("notesDisplay")
+	? JSON.parse(localStorage.getItem("notesDisplay"))
+	: [];
+
+function post() {
+	if (inputNotes.value != "") {
+		// initializing tags to display in edit-note.html
+		var paragraphTag = document.createElement("p");
+		var linkTag = document.createElement("a");
+		var lineBreak = document.createElement("br");
+
+    var input = inputNotes.value + "\t ".repeat(20);
+		// saveNotesToLocalStorage(inputNotes.value); //TODO: include tabs? for now...
+
+		var note = document.createTextNode(input);
+
+		// var notesDisplay = document.getElementById("notesdisplay");
+
+		var toPost = paragraphTag.appendChild(note);
+
+		notesDisplay.appendChild(toPost);
+
+		var currentTime = displayTime.textContent;
+    // saveNotesToLocalStorage(currentTime); // !!!!!!!!!!!!!!!!!!!
+		var timeNode = document.createTextNode(currentTime);
+
+    // saveNotesToLocalStorage(inputNotes.value + " " + currentTime);
+		linkTag.appendChild(timeNode);
+		linkTag.href = "https://github.com/novedevo/notable";
+    saveNotesToLocalStorage(inputNotes.value + " " + linkTag);
+		notesDisplay.appendChild(linkTag);
+		notesDisplay.appendChild(lineBreak);
+		inputNotes.value = "";
+	}
+}
+
+function saveNotesToLocalStorage(value) {
+	notesStorage.push(value);
+	localStorage.setItem("notes", JSON.stringify(notesStorage));
+}
+
+function renderNotes() {
+	const getNotes = JSON.parse(localStorage.getItem("notes"));
+	post();
+}
+
+console.log(localStorage.getItem("notes"));
