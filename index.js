9f6b8571d5165456efb80a51fd171c6b529f0816
Devon Sawatsky <novedevo@gmail.com>
2022-07-08 11:04:10 -0700

    remove old api calls, add jwt validation to express api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
-import session from "express-session";
 import pg from "pg";
+import jwt from "express-jwt";
+import jwks from "jwks-rsa";
 
 //initialize postgres connection
 const { Pool } = pg;
@@ -22,105 +23,36 @@ const __dirname = import.meta.url
 	.replace("file://", "")
 	.replace("/index.js", "");
 
+//setup authentication
+const requiresLogin = jwt({
+	secret: jwks.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: "https://dev--9y96m38.us.auth0.com/.well-known/jwks.json",
+	}),
+	audience: "notable-api",
+	issuer: "https://dev--9y96m38.us.auth0.com/",
+	algorithms: ["RS256"],
+});
+
 //initialize express
 const app = express();
 
-//setup express-session
-app.use(
-	session({
-		secret: "notable", //really should be a truly secure string, but this is fine considering we have zero real adversaries
-		resave: false,
-		saveUninitialized: true,
-		cookie: {
-			maxAge: 1000 * 60 * 60 * 24 * 7 * 2, //two weeks
-		},
-	})
-);
-
-function requiresLogin(req, res, next) {
-	if (req.session.user) {
-		next();
-	} else {
-		res.redirect("/login.html");
-	}
-}
-function requiresAdmin(req, res, next) {
-	if (req.session.admin) {
-		next();
-	} else {
-		res.status(401).send("Unauthorized");
-	}
-}
-
-app.use(express.static("public"));
+app.use(express.static("build")); //serve react application
 app.use(express.urlencoded({ extended: true }));
-
-app.get("/", requiresLogin, (req, res) => {
-	res.sendFile(__dirname + "/secured/dashboard.html");
-});
-
-app.get("/secured/adminConsole.html", requiresAdmin, (req, res) => {
-	const path = __dirname + "/secured/adminConsole.html";
-	res.sendFile(path);
-});
-
-app.get("/secured/:file", requiresLogin, (req, res) => {
-	res.sendFile(__dirname + "/secured/" + req.params.file);
-});
 
 // API section
 
-app.post("/api/login", async (req, res) => {
-	const { username, password } = req.body;
-	const result = await pool.query(
-		"SELECT * FROM users WHERE username = $1 AND password = $2",
-		[username, password]
-	);
-	if (result.rows.length === 0) {
-		res.status(401).send("Invalid username or password");
-	} else {
-		req.session.regenerate(() => {
-			req.session.user = username;
-			req.session.name = result.rows[0].name;
-			req.session.admin = result.rows[0].admin;
-			req.session.save(() => res.redirect("/"));
-		});
-	}
-});
-
-app.get("/logout", requiresLogin, (req, res) => {
-	req.session.destroy(() => res.redirect("/"));
-});
-
-app.get("/api/users", requiresAdmin, async (req, res) => {
-	const result = await pool.query("SELECT username, name, admin FROM users");
-	res.json(result.rows);
-});
-app.get("/api/user_info", requiresLogin, async (req, res) => {
-	const result = await pool.query("SELECT * FROM users WHERE username = $1", [
-		req.session.user,
+app.get("/api/notes", requiresLogin, async (req, res) => {
+	const result = await pool.query("SELECT * FROM notes WHERE username = $1", [
+		req.body.user,
 	]);
 	res.json(result.rows?.[0]);
 });
-app.patch("/api/promote_user", requiresAdmin, async (req, res) => {
-	await pool.query("UPDATE users SET admin = true WHERE username = $1", [
-		req.query.username,
-	]);
-	res.send("User promoted");
-});
-app.patch("/api/demote_user", requiresAdmin, async (req, res) => {
-	await pool.query("UPDATE users SET admin = false WHERE username = $1", [
-		req.query.username,
-	]);
-	res.send("User demoted");
-});
-app.delete("/api/delete_user", requiresAdmin, async (req, res) => {
-	const result = await pool.query("DELETE FROM users WHERE username = $1", [
-		req.query.username,
-	]);
-	if (result.rowCount) {
-		res.send("User deleted");
-	}
+
+app.get("/*", (req, res) => {
+	res.sendFile(__dirname + "/build/index.html");
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
