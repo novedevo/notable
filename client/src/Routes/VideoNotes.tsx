2c504ba61076dc6999785463bae870fb6916d618
Devon Sawatsky <novedevo@gmail.com>
2022-07-08 14:30:16 -0700

    intermediate implementation of videoNotes

--- client/src/Routes/VideoNotes.tsx
+++ client/src/Routes/VideoNotes.tsx
@@ -1,29 +1,34 @@
-import {
-	Button,
-	Container,
-	List,
-	ListItem,
-	TextField,
-	Typography,
-} from "@mui/material";
+import { Button, Container, TextField, Typography } from "@mui/material";
+import React from "react";
+import { useState } from "react";
 import InputNotes from "../components/InputNotes";
 import NotesDisplay from "../components/NotesDisplay";
 import Video from "../components/Video";
 
 export default function VideoNotes() {
+	const [videoUrl, setVideoUrl] = useState("LEENEFaVUzU");
+	const [videoId, setVideoId] = useState(videoUrl);
+	const myRef = React.createRef<Video>();
+	const loadVideo = () => {
+		const id = parseId(videoUrl);
+		if (id) {
+			setVideoId(id);
+		} else {
+			alert("Invalid URL");
+		}
+	};
+
 	return (
 		<>
-			<TextField variant="outlined" id="video-form" label="URL" />
-			<Button>Load Video</Button>
-			TEST URLs YOU CAN COPY AND PASTE:
-			<List>
-				<ListItem>https://www.youtube.com/watch?v=LEENEFaVUzU</ListItem>
-				<ListItem>
-					https://www.youtube.com/watch?v=xMk8wuw7nek&ab_channel=EXCEEDINGSHADOW
-				</ListItem>
-			</List>
+			<TextField
+				variant="outlined"
+				id="video-form"
+				label="URL"
+				onChange={(event) => setVideoUrl(event.target.value)}
+			/>
+			<Button onClick={loadVideo}>Load Video</Button>
 			<Container>
-				<Video />
+				<Video videoId={videoId} ref={myRef} />
 				<div className="right-side">
 					<Typography>Notes</Typography>
 					<NotesDisplay />
@@ -32,4 +37,16 @@ export default function VideoNotes() {
 			</Container>
 		</>
 	);
+}
+
+// Method, YT Parser. Not ours.
+function parseId(url: string) {
+	// Regular expression, all possible combinations before YT unique ID.
+	const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+
+	// Comparison between arg and the regular expression
+	const match = url.match(regExp)?.[2];
+
+	// Return the video ID by itself.
+	return match?.length === 11 ? match : null;
 }
--- client/src/components/Video.tsx
+++ client/src/components/Video.tsx
@@ -1,3 +1,45 @@
-export default function Video() {
-	return <></>;
+import React from "react";
+import { useState } from "react";
+import YouTube, { YouTubePlayer } from "react-youtube";
+
+export default class Video extends React.Component<
+	{ videoId: string },
+	{ player: YouTubePlayer }
+> {
+	constructor({ videoId }: { videoId: string }) {
+		super({ videoId });
+		this.state = {
+			player: null as YouTubePlayer,
+		};
+	}
+	render() {
+		return (
+			<YouTube
+				videoId={this.props.videoId}
+				opts={{
+					height: 800,
+					width: 1000,
+					playerVars: {
+						autoplay: 1,
+						playsInline: 1,
+					},
+				}}
+				onReady={(event) => this.setState({ player: event.target })}
+			/>
+		);
+	}
+	setTime(seconds: number) {
+		if (this.state.player) {
+			this.state.player.seekTo(seconds);
+		} else {
+			alert("Player not ready");
+		}
+	}
+	getTime() {
+		if (this.state.player) {
+			return this.state.player.getCurrentTime();
+		} else {
+			alert("Player not ready");
+		}
+	}
 }
