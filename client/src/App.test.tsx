3612433445b7246e7f919872d3e1d24a48207b2d
Devon Sawatsky <novedevo@gmail.com>
2022-07-08 12:54:24 -0700

    cleanup

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("doesn't render learn react link", () => {
 	render(<App />);
 	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+	expect(linkElement).not.toBeInTheDocument();
 });
