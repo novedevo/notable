d624e2a3d3562e9a0e6513552d83c1d809219353
Devon Burnham <novedevo@gmail.com>
2022-07-09 14:56:28 -0700

    fix browser-side jwt decoding

--- client/src/Routes/Splash.tsx
+++ client/src/Routes/Splash.tsx
@@ -1,13 +1,13 @@
 import Dashboard from "./Dashboard";
 import Login from "./Login";
-import jwt from "jsonwebtoken";
+import jwtDecode from "jwt-decode";
 
 export default function Splash() {
 	let isLoggedIn = false;
 	const token = localStorage.getItem("token");
 	if (token) {
-		const decoded = jwt.decode(token) as jwt.JwtPayload;
-		if (decoded.exp! > Date.now() / 1000) {
+		const decoded = jwtDecode<{ exp: number }>(token);
+		if (decoded.exp > Date.now() / 1000) {
 			isLoggedIn = true;
 		}
 	}
