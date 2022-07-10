305e9b2a1020660809c1b3363ea658a513d2c3e1
Devon Burnham <novedevo@gmail.com>
2022-07-09 21:19:44 -0700

    add registration

--- client/src/Routes/Register.tsx
+++ client/src/Routes/Register.tsx
@@ -1,3 +1,56 @@
+import { Container, TextField, Button } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Register() {
-	return <></>;
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [name, setName] = useState("");
+	const navigate = useNavigate();
+
+	const submit = () => {
+		axios
+			.post("/api/register", { username, password, name })
+			.then((res) => {
+				if (res.data.token) {
+					localStorage.setItem("token", res.data.token);
+					localStorage.setItem(
+						"user",
+						JSON.stringify({ name, username, admin: false })
+					);
+					navigate("/login");
+				}
+			})
+			.catch((err) => alert("invalid username or password"));
+	};
+	return (
+		<Container>
+			<h1>Create a new account</h1>
+			<div>
+				<TextField
+					variant="outlined"
+					label="name"
+					onChange={(event) => setName(event.target.value)}
+				/>
+				<TextField
+					variant="outlined"
+					label="username"
+					onChange={(event) => setUsername(event.target.value)}
+				/>
+				<TextField
+					variant="outlined"
+					label="password"
+					type="password"
+					onChange={(event) => setPassword(event.target.value)}
+				/>
+				<Button variant="contained" onClick={submit}>
+					Register
+				</Button>
+				<Button variant="contained" href="/login">
+					Log in with an existing account
+				</Button>
+			</div>
+		</Container>
+	);
 }
