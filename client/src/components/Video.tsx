0b5842ccdfab6bd6a9aea66aa164817230cd2c8a
Devon Burnham <novedevo@gmail.com>
2022-07-09 21:07:48 -0700

    try converting video to function

--- client/src/components/Video.tsx
+++ client/src/components/Video.tsx
@@ -1,45 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import YouTube, { YouTubePlayer } from "react-youtube";
 
-export default class Video extends React.Component<
-	{ videoId: string },
-	{ player: YouTubePlayer }
-> {
-	constructor({ videoId }: { videoId: string }) {
-		super({ videoId });
-		this.state = {
-			player: null as YouTubePlayer,
-		};
-	}
-	render() {
-		return (
-			<YouTube
-				videoId={this.props.videoId}
-				opts={{
-					height: 800,
-					width: 1000,
-					playerVars: {
-						autoplay: 1,
-						playsInline: 1,
-					},
-				}}
-				onReady={(event) => this.setState({ player: event.target })}
-			/>
-		);
-	}
-	setTime(seconds: number) {
-		if (this.state.player) {
-			this.state.player.seekTo(seconds);
-		} else {
-			alert("Player not ready");
+export default function Video({
+	videoId,
+	time,
+}: {
+	videoId: string;
+	time: number;
+}) {
+	const [player, setPlayer] = useState<YouTubePlayer>(null);
+	useEffect(() => {
+		if (player) {
+			player.seekTo(time);
+			player.playVideo();
 		}
-	}
-	getTime() {
-		if (this.state.player) {
-			return this.state.player.getCurrentTime();
-		} else {
-			alert("Player not ready");
-		}
-	}
+	});
+	useEffect(() => {
+		player.g;
+	});
+
+	return (
+		<YouTube
+			videoId={videoId}
+			opts={{
+				height: 800,
+				width: 1000,
+				playerVars: {
+					// autoplay: 1,
+					playsInline: 1,
+					modestBranding: 1,
+					start: time,
+				},
+			}}
+			onReady={(event) => setPlayer(event.target)}
+		/>
+	);
 }
