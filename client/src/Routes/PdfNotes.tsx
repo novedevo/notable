3dde92f16ba2023d8bc0db1baccebd85f8d27089
Devon Burnham <novedevo@gmail.com>
2022-07-09 22:37:09 -0700

    fix pdfNotes.tsx

--- client/src/Routes/PdfNotes.tsx
+++ client/src/Routes/PdfNotes.tsx
@@ -1,24 +1,29 @@
-import { Button, Container, TextField, Typography } from "@mui/material";
-import { useState } from "react";
+import { Button, Card, Container, TextField, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import InputNotes from "../components/InputNotes";
 import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import relativeTime from "dayjs/plugin/relativeTime";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
-import _ from "lodash";
+
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
 
 export default function PdfNotes() {
-	const [notes, setNotes] = useState<string[]>([]);
+	const [notes, setNotes] = useState<[string, number, number][]>([]);
 	const [date, setDate] = useState(dayjs());
-	const [time, setTime] = useState("");
-	setInterval(() => {
-		let diff = dayjs().diff(date, "second");
-		let neg = diff < 0;
-		setTime(
-			dayjs(Math.abs(diff)).format("HH:mm:ss") + neg ? "from now" : "ago"
-		);
-	}, 1000);
+	const [time, setTime] = useState(date.format("HH:mm:ss"));
+	useEffect(() => {
+		const interval = setInterval(() => {
+			let diff = date.diff(dayjs());
+			setTime(dayjs.duration(diff, "millisecond").humanize(true));
+		}, 1000);
+
+		return () => clearInterval(interval);
+	});
 
 	const [numPages, setNumPages] = useState(1);
-	const [pageNumber, setPageNumber] = useState(1);
+	const [pageNumber, setPageNumber] = useState(0);
 	const [file, setFile] = useState<File | null>(null);
 
 	const inc = () => {
@@ -51,15 +56,20 @@ export default function PdfNotes() {
 			<div id="container">
 				<Document
 					file={file}
-					onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+					onLoadSuccess={({ numPages }) => {
+						setNumPages(numPages);
+						setPageNumber(1);
+					}}
 				>
-					{_.range(numPages).map((i) => (
-						<Page pageNumber={i + 1} />
-					))}
+					<Page
+						pageNumber={pageNumber || 1}
+						renderTextLayer={false} //https://github.com/wojtekmaj/react-pdf/issues/332
+					/>
 				</Document>
 
 				<div className="right-side">
 					<Container>
+						<br />
 						<TextField
 							label="Beginning of Presentation"
 							type="datetime-local"
@@ -70,26 +80,36 @@ export default function PdfNotes() {
 						/>
 						{time}
 					</Container>
-					<Container>
-						{notes.map((note) => generateNote(note, date, pageNumber))}
-					</Container>
-					<InputNotes post={(note) => setNotes([...notes, note])} />
+					<Container>{notes.map(generateNote)}</Container>
+					<InputNotes
+						post={(note) => {
+							const diff = dayjs().diff(date);
+							if (diff > 0 && pageNumber > 0) {
+								setNotes([...notes, [note, pageNumber, diff]]);
+							} else if (pageNumber > 0) {
+								alert("You can't post notes until the presentation starts");
+							} else {
+								alert("Please load a PDF to begin taking notes");
+							}
+						}}
+					/>
 				</div>
 			</div>
 		</Container>
 	);
 }
 
-function generateNote(note: string, date: dayjs.Dayjs, page: number) {
-	const diff = dayjs().diff(date, "second");
-	if (diff < 0) {
-		alert("You can't add notes until the presentation has started");
-	}
+function generateNote(
+	[note, page, time]: [string, number, number],
+	index: number
+) {
 	return (
-		<Container>
+		<Card key={index}>
 			<Typography>{note}</Typography>
-			<Typography>{dayjs(diff).format("HH:mm:ss")}</Typography>
+			<Typography>
+				{dayjs.duration(time, "milliseconds").format("HH:mm:ss")}
+			</Typography>
 			<Typography>Page {page}</Typography>
-		</Container>
+		</Card>
 	);
 }
