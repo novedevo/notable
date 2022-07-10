0adc7697dedf1773453b620f604f672b9a3e8355
Devon Burnham <novedevo@gmail.com>
2022-07-09 21:19:29 -0700

    add links to pdf and video viewers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import Login from "./Routes/Login";
 import Register from "./Routes/Register";
 import Splash from "./Routes/Splash";
 import Console from "./Routes/Console";
+import VideoNotes from "./Routes/VideoNotes";
+import PdfNotes from "./Routes/PdfNotes";
 
 export default function App() {
 	return (
@@ -13,6 +15,8 @@ export default function App() {
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/console" element={<Console />} />
+				<Route path="/edit" element={<VideoNotes />} />
+				<Route path="/pdf" element={<PdfNotes />} />
 			</Routes>
 		</BrowserRouter>
 	);
