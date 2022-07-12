db9551c8fe2ff37d05d78df4e2a2063da75612dc
Devon Burnham <novedevo@gmail.com>
2022-07-12 13:55:08 -0700

    switch to cdn-based pds.js to resolve OOM in heroku build script

--- client/src/routes/PdfNotes.tsx
+++ client/src/routes/PdfNotes.tsx
@@ -4,7 +4,8 @@ import InputNotes from "../components/InputNotes";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
