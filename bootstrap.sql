b221fcfe1da2b9cb89a846ec245ade0ac91a11dc
Devon Burnham <novedevo@gmail.com>
2022-07-19 14:25:57 -0700

    add constraints and an example presentation

--- bootstrap.sql
+++ bootstrap.sql
@@ -1,27 +1,30 @@
 DROP TABLE users CASCADE;
-CREATE TABLE users ( 
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     name TEXT NOT NULL,
     admin BOOLEAN NOT NULL DEFAULT FALSE
 );
-
 INSERT INTO users (username, password, name, admin)
 VALUES ('admin', 'admin', 'Admin', TRUE);
 INSERT INTO users (username, password, name, admin)
 VALUES ('user', 'user', 'User', FALSE);
-
 DROP TABLE presentations CASCADE;
 CREATE TABLE presentations (
     presentation_instance_id SERIAL PRIMARY KEY,
-    title TEXT,
+    title TEXT NOT NULL,
     scheduled_date TIMESTAMP(0),
     youtube_url TEXT,
     pdf TEXT,
-    presenter_id integer REFERENCES users (id)
+    presenter_id integer REFERENCES users (id) NOT NULL
 );
-
+INSERT INTO presentations (title, scheduled_date, presenter_id)
+VALUES (
+        'Example Presentation',
+        '2022-07-19 10:00:00',
+        1
+    );
 DROP TABLE notes CASCADE;
 CREATE TABLE notes (
     note_id SERIAL PRIMARY KEY,
@@ -31,3 +34,16 @@ CREATE TABLE notes (
     notetaker_id integer REFERENCES users (id),
     presentation_id integer REFERENCES presentations (presentation_instance_id)
 );
+INSERT INTO notes (
+        note,
+        time_stamp,
+        page_number,
+        notetaker_id,
+        presentation_id
+    )
+VALUES (
+        'This is an example note',
+        '2022-07-19 10:05:00',
+        2,
+        1
+    );
