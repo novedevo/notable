6961547588659d619fcb2ad8b1474ef88d58b911
Devon Sawatsky <novedevo@gmail.com>
2022-07-18 20:20:09 -0700

    prettier reformat

--- client/src/routes/ViewNotes.tsx
+++ client/src/routes/ViewNotes.tsx
@@ -3,35 +3,34 @@ import react from "react";
 import { useEffect, useState } from "react";
 
 const ViewNotes = () => {
+	const [notes, setNotes] = useState<any[]>([]);
+	useEffect(() => {
+		fetchNotes();
+	}, []);
 
-    const [notes, setNotes] = useState<any[]>([]);
-    useEffect(() => {
-        fetchNotes();
-    }, []);
+	const fetchNotes = () => {
+		axios
+			.get("/api/get_noteSet")
+			.then((res) => {
+				console.log(res);
+				setNotes(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-    const fetchNotes = () => {
-        axios
-        .get("/api/get_noteSet")
-        .then((res) => {
-            console.log(res);
-            setNotes(res.data);
-        })
-        .catch((err => {
-            console.log(err);
-        }))
-    }
-
-    return (
-        <div>
-            <h1>View Notes</h1>
-            <div id="noteSets_container">
-                {notes.map((note) => (
-                    <a id="noteSet" href="javascript:0">
-                        <p>{note.title}</p>
-                    </a>
-                ))}
-            </div>
-        </div>
-    )
-}
-export default ViewNotes
+	return (
+		<div>
+			<h1>View Notes</h1>
+			<div id="noteSets_container">
+				{notes.map((note) => (
+					<a id="noteSet" href="javascript:0">
+						<p>{note.title}</p>
+					</a>
+				))}
+			</div>
+		</div>
+	);
+};
+export default ViewNotes;
