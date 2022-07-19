b1d7dc67dd8d5a06c61fa1ec90d4514ed445c29c
Devon Sawatsky <novedevo@gmail.com>
2022-07-18 21:19:06 -0700

    fix Presentations.tsx

--- client/src/routes/Presentations.tsx
+++ client/src/routes/Presentations.tsx
@@ -5,75 +5,57 @@ import { useNavigate } from "react-router-dom";
 import { io } from "socket.io-client";
 import DashboardButton from "../components/DashboardButton";
 
+const client = axios.create({
+	headers: {
+		Authorization: `Bearer ${localStorage.getItem("token")}`,
+	},
+});
+
 // creates the socket endpoint so that we can emit messages to the server
 const socket = io();
-socket.on("connect_error", (err: { message: any }) => {
+socket.on("connect_error", (err) => {
 	console.log(`connect_error due to ${err.message}`);
 });
 
 export default function Presentations() {
 	const navigate = useNavigate();
-	const userJson = localStorage.getItem("user");
-	const [presentationID, setPresentationID] = useState("");
-	const [databasePresentations, setDatabasePresentations] = useState<any[]>([]);
-	const [userPresentations, setuserPresentations] = useState<any[]>([]);
-	const [presentationHost, setPresentationHost] = useState("");
-
-	let user: { name?: any };
-	try {
-		user = JSON.parse(userJson!);
-	} catch (err) {
-		user = {};
-	}
-
-	// setting the id of the host
-	useEffect(() => {
-		getUserId().then((id) => {
-			setPresentationHost(id);
-		});
-	}, []);
-
-	// checks against the localstorage of presentations if there is a valid presentation corresponding to the name
-	const validPresentationId = () => {
-		let validCode = false;
-		databasePresentations.forEach((presentation) => {
-			if (presentationID === presentation.presentation_instance_id) {
-				validCode = true;
-				joinPresentation();
-			}
-		});
-		if (validCode === false) {
-			alert("Not a valid room code");
-		}
-	};
-
-	// sends userData to the server so that a person can join a room and sends the user to that room
-	const joinPresentation = () => {
-		const userData = {
-			room: presentationID,
-			name: user.name,
-		};
-		socket.emit("join_room", userData);
-		navigate("/room/" + presentationID);
-	};
+	const user: User = JSON.parse(localStorage.getItem("user")!);
+	const [presentationID, setPresentationID] = useState(0);
+	const [dbPresentations, setDbPresentations] = useState<Presentation[]>([]);
+	const [userPresentations, setUserPresentations] = useState<Presentation[]>(
+		[]
+	);
 
 	// Database Presentations
 	useEffect(() => {
-		getPresentations().then((presentation) => {
-			setDatabasePresentations(presentation);
+		getPresentations().then((presentations) => {
+			setDbPresentations(presentations);
+			setUserPresentations(
+				presentations.filter(
+					(presentation) => presentation.presenter_id === user.id
+				)
+			);
 		});
-	}, []);
+	}, [user.id]);
 
-	useEffect(() => {
-		databasePresentations.forEach((presentation) => {
-			if (presentationHost === presentation.presenter_id) {
-				setuserPresentations((usersPresentations2) => [
-					...usersPresentations2,
-					presentation,
-				]);
-			}
-		});
-	}, [databasePresentations]);
+	const joinRoom = () => {
+		// checks against the database of presentations if there is a valid presentation corresponding to the id
+		const validCode = dbPresentations.some(
+			(presentation) => presentation.presentation_instance_id === presentationID
+		);
+		if (validCode) {
+			const userData = {
+				room: presentationID,
+				name: user.name,
+			};
+			// sends userData to the server so that a person can join a room
+			socket.emit("join_room", userData);
+			// sends the user to that room
+			navigate("/room/" + presentationID);
+		} else {
+			alert("Not a valid room code");
+		}
+	};
 
 	return (
 		<Container>
@@ -87,20 +69,18 @@ export default function Presentations() {
 				variant="outlined"
 				id="PresentationID"
 				label="Presentation ID"
-				onChange={(event) => {
-					setPresentationID(event.target.value);
-				}}
+				onChange={(event) => setPresentationID(parseInt(event.target.value))}
 			/>
-			<Button href="" variant="contained" onClick={validPresentationId}>
+			<Button href="" variant="contained" onClick={joinRoom}>
 				Join Presentation
 			</Button>
 			<Container id="displayPresentations">
 				{userPresentations.map((presentation) => (
 					<Card>
 						<li>{presentation.title}</li>
-						<li>Host: {presentation.presenter_id}</li>
+						<li>Host ID: {presentation.presenter_id}</li>
 						<li>Starts at: {presentation.scheduled_date}</li>
-						<li>Join with: {presentation.presentation_instance_id}</li>
+						<li>Join with code: {presentation.presentation_instance_id}</li>
 					</Card>
 				))}
 			</Container>
@@ -108,25 +88,28 @@ export default function Presentations() {
 	);
 }
 
-async function getPresentations() {
+async function getPresentations(): Promise<Presentation[]> {
 	try {
-		const result = await axios("/api/presentations");
+		const result = await client("/api/presentations");
 		console.log(result.data.presentations);
 		return result.data.presentations;
 	} catch (err) {
-		console.log(err);
+		console.error("Error getting presentations");
+		console.error(err);
+		return [];
 	}
 }
 
-async function getUserId() {
-	try {
-		const result = await axios("/api/user_id", {
-			headers: {
-				Authorization: `Bearer ${localStorage.getItem("token")}`,
-			},
-		});
-		return result.data.id;
-	} catch (err) {
-		console.log(err);
-	}
-}
+type Presentation = {
+	presentation_instance_id: number;
+	title: string;
+	presenter_id: number;
+	scheduled_date: string;
+};
+
+type User = {
+	id: number;
+	username: string;
+	isAdmin: boolean;
+	name: boolean;
+};
