3c7d6cee457982f910574fab3f669f6533c3bcc9
tchua <tchua@sfu.ca>
2022-07-25 17:33:09 -0700

    Added CSS to Dashboard.

--- client/src/routes/Dashboard.tsx
+++ client/src/routes/Dashboard.tsx
@@ -1,14 +1,26 @@
 import { Button, Container } from "@mui/material";
 import LogoutButton from "../components/LogoutButton";
+import "./AppExtras.css";
 
 export default function Dashboard() {
 	const user = JSON.parse(localStorage.getItem("user")!);
 
 	return (
 		<Container>
-			<h1>Welcome, {user.name}!</h1>
+			<div id="background-image"></div>
+			<div>
+				<div id="pageHead">
+				<h1>Welcome, {user.name}!</h1>
+				</div>
+			
+			<div id="middlePanel">
+				<br>
+				</br>
+			<h2>Dashboard</h2>
+			
 			{user.isAdmin && (
 				<Button
+					id="buttonSecondary"
 					href="/console"
 					variant="contained"
 					sx={{
@@ -20,8 +32,8 @@ export default function Dashboard() {
 					Admin Console
 				</Button>
 			)}
-			<LogoutButton />
 			<Button
+				id="buttonPrimary"
 				href="/presentations"
 				variant="contained"
 				sx={{
@@ -32,7 +44,10 @@ export default function Dashboard() {
 			>
 				Presentations
 			</Button>
+			<br>
+			</br>
 			<Button
+				id="buttonPrimary"
 				href="/view"
 				variant="contained"
 				sx={{
@@ -43,6 +58,11 @@ export default function Dashboard() {
 			>
 				My Notes
 			</Button>
+			<br>
+			</br>
+			<LogoutButton/>
+			</div>
+			</div>
 		</Container>
 	);
 }
