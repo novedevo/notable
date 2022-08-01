6497ec4cf3538eb8b39f8c9be3dee90d82897007
tchua <tchua@sfu.ca>
2022-07-31 23:01:51 -0700

    test

--- client/src/routes/Presentations.tsx
+++ client/src/routes/Presentations.tsx
@@ -152,7 +152,6 @@ export default function Presentations() {
 						<TextField
 							style={{
 								backgroundColor: "white",
-								marginTop: "3%",
 							}}
 							variant="outlined"
 							id="PresentationID"
