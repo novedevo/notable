fc0a8b879700d92f2125a6665d23bb9723443b8e
tchua <tchua@sfu.ca>
2022-08-03 22:16:38 -0700

    nudge presentations page buttons

--- client/src/routes/Presentations.tsx
+++ client/src/routes/Presentations.tsx
@@ -189,14 +189,6 @@ export default function Presentations() {
 									{presentation.presentation_end_date != null && (
 										<div>This Presentation has ended</div>
 									)}
-									{presentation.pdf != null &&
-										presentation.presentation_end_date != null && (
-											<Button
-												id="mediabutton"
-												value={presentation.presentation_instance_id}
-												onClick={changeNote}
-											></Button>
-										)}
 									<Button
 										id="deletebutton"
 										onClick={() =>
@@ -221,6 +213,14 @@ export default function Presentations() {
 											navigate("/room/" + presentation.presentation_instance_id)
 										}
 									></Button>
+									{presentation.pdf != null &&
+										presentation.presentation_end_date != null && (
+											<Button
+												id="mediabutton"
+												value={presentation.presentation_instance_id}
+												onClick={changeNote}
+											></Button>
+										)}
 								</Card>
 							))}
 						</Container>
